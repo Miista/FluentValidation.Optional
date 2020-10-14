fcf1e633d00852fd63428b0c1fd1666051c5d956
Søren Guldmund <sgu@immeo.dk>
2020-10-14 15:13:59 +0200

    Initial commit

---
+++ FluentValidation.Optional.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentValidation.Optional.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
